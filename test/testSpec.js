67d1ea51590fdc4071b59270ccfc3d4018c627d0
Joshua Thornton <joshua@techinasia.com>
2015-11-17 14:38:49 +0800

    Moved each click out into its own class to allow for cleaner timeouts.

--- test/testSpec.js
+++ test/testSpec.js
@@ -1,13 +1,49 @@
 import Track from '../lib/Track';
 import $ from 'jquery';
+import sinon from 'sinon';
 
-describe('Test', function() {
-  it('Should do this', function() {
-    var $link = $('<a/>');
-    $(document.body).append($link);
+describe('lib/Track', function() {
+  describe('When opening in current window', function() {
+    it('Should not callback', function(done) {
+      var $link = $('<a/>');
+      $link.prop('href', 'http://example.com/')
+        .prop('innerHTML', 'Example');
+      $(document.body).append($link);
 
-    var tracker = new Track();
+      var callbackSpy = sinon.spy();
 
-    $link.click();
+      var tracker = new Track(callbackSpy);
+
+      $link.click();
+      $(window).trigger('unload');
+
+      setTimeout(function() {
+        expect(callbackSpy.called).toBe(true);
+        expect(callbackSpy.args[0][0].newtab).toBe(false);
+        done();
+      }, 1000);
+    });
+  });
+
+  describe('When opening in a new tab', function() {
+    it('Should callback', function(done) {
+      var $link = $('<a/>');
+      $link.prop('href', 'http://example.com/')
+        .prop('target', '_blank')
+        .prop('innerHTML', 'Example');
+      $(document.body).append($link);
+
+      var callbackSpy = sinon.spy();
+
+      var tracker = new Track(callbackSpy);
+
+      $link.click();
+
+      setTimeout(function() {
+        expect(callbackSpy.called).toBe(true);
+        expect(callbackSpy.args[0][0].newtab).toBe(true);
+        done();
+      }, 1000);
+    });
   });
 });
